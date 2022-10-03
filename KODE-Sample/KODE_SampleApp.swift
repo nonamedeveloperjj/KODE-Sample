e46d1df2818619e3cf34d5136ac7d0dd4cbf9fd4
nonamedeveloperjj <100957242+nonamedeveloperjj@users.noreply.github.com>
2022-10-03 17:14:48 +0300

    Create Xcode project

---
+++ KODE-Sample/KODE_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KODE_SampleApp.swift
+//  KODE-Sample
+//
+//  Created by John Snow on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct KODE_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
