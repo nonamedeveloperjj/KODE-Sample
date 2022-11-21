ef0f3243fa93a4115f814f85e2c40227dca59021
nonamedeveloperjj <100957242+nonamedeveloperjj@users.noreply.github.com>
2022-11-21 14:25:57 +0300

    Add error state to EmployeesListModule

--- KODE-Sample/PresentationLayer/Modules/EmployeesListModule/EmployeesListView.swift
+++ KODE-Sample/PresentationLayer/Modules/EmployeesListModule/EmployeesListView.swift
@@ -12,24 +12,42 @@ struct EmployeesListView: View {
     @State private var enteredText = ""
     @FocusState private var isSearchBarFocused: Bool
     
+    private var errorView: some View {
+        var errorView = EmployeesListErrorView()
+        errorView.onRetry = {
+            viewModel.fetchEmployees()
+        }
+        return errorView
+    }
+    
+    private var listView: some View {
+        VStack {
+            CommonSearchBar(enteredText: $enteredText, isFocused: $isSearchBarFocused)
+            List(viewModel.employees.filter({
+                viewModel.isIncluded(employee: $0, enteredText: enteredText)
+            })) { employee in
+                EmployeesListRowView(employee: employee)
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(EdgeInsets())
+                    .redacted(reason: viewModel.isLoadingEmployees ? .placeholder : [])
+            }.refreshable {
+                viewModel.fetchEmployees()
+            }.onAppear {
+                viewModel.fetchEmployees()
+            }.gesture(DragGesture().onChanged({ _ in
+                isSearchBarFocused = false
+            }))
+            .listStyle(.plain)
+            .ignoresSafeArea()
+        }
+    }
+    
     var body: some View {
-        CommonSearchBar(enteredText: $enteredText, isFocused: $isSearchBarFocused)
-        List(viewModel.employees.filter({
-            viewModel.isIncluded(employee: $0, enteredText: enteredText)
-        })) { employee in
-            EmployeesListRowView(employee: employee)
-                .listRowSeparator(.hidden)
-                .listRowInsets(EdgeInsets())
-                .redacted(reason: viewModel.isLoadingEmployees ? .placeholder : [])
-        }.refreshable {
-            viewModel.fetchEmployees()
-        }.onAppear {
-            viewModel.fetchEmployees()
-        }.gesture(DragGesture().onChanged({ _ in
-            isSearchBarFocused = false
-        }))
-        .listStyle(.plain)
-        .ignoresSafeArea()
+        if viewModel.fetchEmployeesError == nil {
+            listView
+        } else {
+            errorView
+        }
     }
     
     init(viewModel: EmployeesListViewModel) {
