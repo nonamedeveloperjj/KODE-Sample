448144917b5994119e0366dd77fce5dd56c1f378
nonamedeveloperjj <100957242+nonamedeveloperjj@users.noreply.github.com>
2022-11-30 18:35:45 +0300

    Add onFirstAppear ViewModifier

---
+++ KODE-Sample/PresentationLayer/Extensions/View+OnFirstAppear.swift
@@ -0,0 +1,32 @@
+//
+//  View+OnFirstAppear.swift
+//  KODE-Sample
+//
+//  Created by John Snow on 23/11/2022.
+//
+
+import SwiftUI
+
+public extension View {
+    func onFirstAppear(_ action: @escaping () -> ()) -> some View {
+        modifier(FirstAppear(action: action))
+    }
+}
+
+private struct FirstAppear: ViewModifier {
+    private let action: () -> ()
+    
+    @State private var hasAppeared = false
+    
+    internal init(action: @escaping () -> ()) {
+        self.action = action
+    }
+    
+    func body(content: Content) -> some View {
+        content.onAppear {
+            guard !hasAppeared else { return }
+            hasAppeared = true
+            action()
+        }
+    }
+}
